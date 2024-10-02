1c3a70bb9f4d467aab415f9b88208fe29fb937c7
Al-Anher Provat <78719429+alanherprovat@users.noreply.github.com>
2024-10-02 18:28:10 +0600

    updates

--- src/components/commonComponents/customCard.jsx
+++ src/components/commonComponents/customCard.jsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const CustomCard = ({ children, style }) => {
   return (
-    <div className="card p-3 mb-3 w-100" style={{ borderColor: '#A9B8CEB2', borderWidth: '0.8px', ...style }}>
+    <div className="card p-2 mb-3 w-100" style={{ borderColor: '#A9B8CEB2', borderWidth: '0.8px', ...style }}>
      {children}
     </div>
   
---
+++ src/components/serviceComponents/ipoRequests/ipoList.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import CustomCard from '../../commonComponents/customCard'
+import ipoStore from '../../../store/ipoStore';
+import '../../../assets/css/ipoRequestStyle/ipoList.css';
+import CustomCheckbox from '../../commonComponents/CustomCheckbox';
+
+export default function IpoList({ selectedItems, multipleSelect, handleSelect, setItemArray }) {
+    const {
+        ipoClientFinancialInfo,
+        setIpoClientFinancialInfo,
+        onChangeIpoClientFinancialInfo,
+        resetIpoClientFinancialInfo
+        }=ipoStore();
+  return (
+         <CustomCard  style={{
+            height: '30vh', // Use 'vh' for height instead of Dimensions
+            borderRadius: '6px',
+            borderWidth: '0.3px',
+            borderColor: '#CAC7C9',
+            borderStyle: 'solid', // Ensure border is visible
+        }}
+          >
+            <div className="col-container">
+                <h5 className="label-text-eligible-ipo">Eligible IPO List</h5>
+            </div>
+
+            <div className="ipo-list-container">
+                {ipoClientFinancialInfo?.IPOEligableClientList.map((item, index) => (
+                    <div key={index} className="ipo-list-row">
+                        
+                        <div className="col-4 instrument-container">
+                            <div className="d-flex align-items-center main-row-content">
+                                <span className="security-label">{item?.SecurityCode}</span>
+                                {multipleSelect && (
+                                    <CustomCheckbox                                       
+                                        checked={selectedItems[index] || false}
+                                        onPress={() => handleSelect(index, item)}
+                                        onboarding={false}
+                                        containerStyle='checkbox-container'
+                                        // textStyle='checkbox-textStyle'
+                                    />
+
+
+                                    // <input
+                                    //     type="checkbox"
+                                    //     checked={selectedItems[index] || false}
+                                    //     onChange={() => handleSelect(index, item)}
+                                    //     className="form-check-input custom-checkbox ms-2"
+                                    // />
+
+                                )}
+                            </div>
+                            <span className="instrument-name">({item?.InstrumentName})</span>
+                        </div>
+
+
+                        <div className="col-4 date-container">
+                            <span className="subs-date-label">Subscription Date</span>
+                            <span className="date-text">{item?.SubscriptionOpeningDate}</span>
+                        </div>
+
+
+                        <div className="col-4 button-container">
+                            <button
+                                className={`apply-button ${multipleSelect ? 'disabled-button' : ''}`}
+                                onClick={() => {
+                                    console.log('Button Pressed', item);
+                                    setItemArray([item]);
+                                }}
+                                disabled={multipleSelect}
+                            >
+                                Apply
+                            </button>
+                        </div>
+
+                    </div>
+                ))}
+            </div>
+
+
+         </CustomCard>
+    )
+}
