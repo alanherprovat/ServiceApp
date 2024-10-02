ce231fa5733fee49e2be22221a4d294aa3723d0f
Al-Anher Provat <78719429+alanherprovat@users.noreply.github.com>
2024-10-02 11:47:40 +0600

    updates

--- src/components/commonComponents/customCard.jsx
+++ src/components/commonComponents/customCard.jsx
@@ -3,9 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const CustomCard = ({ children, style }) => {
   return (
-    <div className="card p-3 mb-3 mx-4" style={{ borderColor: '#A9B8CEB2', borderWidth: '0.8px', ...style }}>
-      {children}
+    <div className="card p-3 mb-3 w-100" style={{ borderColor: '#A9B8CEB2', borderWidth: '0.8px', ...style }}>
+     {children}
     </div>
+  
   );
 };
 
