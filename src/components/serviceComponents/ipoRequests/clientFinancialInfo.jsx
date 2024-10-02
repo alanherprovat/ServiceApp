38ebdc8caaa020534cf1319e54f6a51e440c717a
Al-Anher Provat <78719429+alanherprovat@users.noreply.github.com>
2024-10-02 13:52:41 +0600

    updates adding checkbox

--- src/components/serviceComponents/ipoRequests/clientFinancialInfo.jsx
+++ src/components/serviceComponents/ipoRequests/clientFinancialInfo.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import {IpoClientFinancialInfo} from  '../../../ApiRequests/ipoRequest'
 import ipoStore from '../../../store/ipoStore';
 import CustomCard from '../../commonComponents/customCard';
+import  '../../../assets/css/ipoRequestStyle/ipoStyle.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ClientFinancialInfo = ({ style }) => {
   const {
@@ -19,17 +21,20 @@ const ClientFinancialInfo = ({ style }) => {
 
   return (
     <CustomCard>
-      <div className="row justify-content-around align-items-center">
-        <div className="col text-center">
-          <p className="font-weight-bold text-dark">Available Balance</p>
-          <p className="font-weight-bold">{Number(ipoClientFinancialInfo?.availableBalance)?.toFixed(2)}</p>
-        </div>
-        <div className="col text-center">
-          <p className="font-weight-bold text-dark">Purchase Power</p>
-          <p className="font-weight-bold">{Number(ipoClientFinancialInfo?.purchasePower)?.toFixed(2)}</p>
-        </div>
+  
+    <div className="row justify-content-around align-items-center">
+      <div className="col-6 text-center">
+        <p className="label-text">Available Balance</p>
+        <p className="value-text">{Number(ipoClientFinancialInfo?.availableBalance)?.toFixed(2)}</p>
       </div>
-    </CustomCard>
+      <div className="col-6 text-center"> {/* Changed to col-6 for equal width */}
+        <p className="label-text">Purchase Power</p>
+        <p className="value-text">{Number(ipoClientFinancialInfo?.purchasePower)?.toFixed(2)}</p>
+      </div>
+    </div>
+
+</CustomCard>
+
   );
 };
 
--- src/components/serviceComponents/ipoRequests/home.jsx
+++ src/components/serviceComponents/ipoRequests/home.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import ClientFinancialInfo from './clientFinancialInfo'
+import CustomCheckbox from '../../commonComponents/CustomCheckbox'
+import '../../../assets/css/commonStyle/checkBox.css';
 
 export default function home() {
+  const [multipleSelect, setMultipleSelect] = useState(false);
   return (
     <>
       <div>home page IPO</div>
       <div>ClientFinancialInfo</div>
       <ClientFinancialInfo/>
+      <CustomCheckbox
+        label={"Select Multiple"}
+        checked={multipleSelect}
+        onPress={() => setMultipleSelect(!multipleSelect)}
+        onboarding={false}
+        // containerStyle={{
+        //       backgroundColor: "transparent",
+        //       padding: 0,
+        //       margin: 0,
+        //       borderWidth: 0,
+        //     }}
+        // textStyle={ {
+        //       color: '#152035B2', // Convert `background` to `backgroundColor`
+        //       fontFamily: 'Inter', // Font family
+        //       fontSize: 12, // Font size in px
+        //       fontWeight: '600', // Font weight
+        //       lineHeight: 12.1, // Line height
+        //       letterSpacing: 0.02, // Letter spacing (in em units will convert to the same float value)
+        //       textAlign: 'left', // Text alignment
+        //       }
+        //       }
+        containerStyle='checkbox-container'
+        textStyle='checkbox-textStyle'
+      />
+        
     </>
   
   )
