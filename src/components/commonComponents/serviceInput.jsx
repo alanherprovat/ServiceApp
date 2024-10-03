e79a0433a02884ad2289c2790d3dafe7740275cd
jamilRez53 <jamilurreza53@gmail.com>
2024-10-02 21:20:36 -0700

    ............

--- src/components/commonComponents/serviceInput.jsx
+++ src/components/commonComponents/serviceInput.jsx
@@ -10,7 +10,8 @@ const ServiceInput = (props) => {
     rightIconPress = () => {},
     className = '',
     value = '',
-    onChange = () => {}
+    onChange = () => {},
+    style
   } = props;
 
   const [focusStyle, setFocusStyle] = useState(false);
@@ -25,7 +26,7 @@ const ServiceInput = (props) => {
 
   return (
     <div className={props.className}>
-      <InputGroup className={`${className}`}>
+      <InputGroup className={`${className}`} style={props.style}>
         {leftIcon && (
           <Button variant="outline-secondary" onClick={leftIconPress}>
             <i className={`bi bi-${leftIcon}`}></i> {/* Bootstrap Icons */}
