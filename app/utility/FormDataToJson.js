a483e87eb151b08c2dca2429b232eccdde7567ea
Al-Anher Provat <alanherprovat3@gmail.com>
2024-10-08 18:25:08 +0600

    updated

--- app/utility/FormDataToJson.js
+++
@@ -1,7 +0,0 @@
-export function formDataToJSON(formData) {
-    const jsonObject = {};
-    formData.forEach(function(value, key){
-        jsonObject[key] = value;
-    });
-    return jsonObject;
-}
